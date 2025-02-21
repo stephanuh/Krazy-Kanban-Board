ddcca7d974e7ab4ea361a2f53f6f9e209061e20f
Papaya <stephaniesosa02@gmail.com>
2025-02-20 20:52:58 -0700

    connected the servers

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -1,10 +1,35 @@
 import { Router, Request, Response } from 'express';
 import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt'; //password hashing
 
+// TODO: If the user exists and the password is correct, return a JWT token
 export const login = async (req: Request, res: Response) => {
-  // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+  let user;
+
+  try {
+    user = await User.findOne({ where: { username }, });
+  } catch (err) {
+    return res.status(500).json({ message: 'Server error' });
+  } if (!user) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+
+  let validPassword;
+
+  try {
+    validPassword = await bcrypt.compare(password, user.password);
+  } catch (err) {
+    return res.status(500).json({ message: 'Server error' });
+  } 
+  if (!validPassword) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+  const secretKey = process.env.JWT_SECRET_KEY || ''
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+  
+  return res.json({ token });
 };
 
 const router = Router();
