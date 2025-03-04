33ead3504dcca36bcb591e9a0960162cbd4bc22e
Papaya <stephaniesosa02@gmail.com>
2025-03-03 21:02:45 -0700

    auth-routes changes

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -7,26 +7,39 @@ import bcrypt from 'bcrypt'; //password hashing
 export const login = async (req: Request, res: Response) => {
    const { username, password } = req.body;
 
-   const user = await User.findOne({
-     where: { username },
-   })
- if (!user) {
-     return res.status(401).json ({ message: 'Authentication failed' });
- }
-
- const passwordIsValid = await bcrypt.compare (password, user.password);
-  if (!passwordIsValid) {
-    return res.status(401).json ({ message: 'Authentication failed' });
+  let user;
+  
+  try {
+    user = await User.findOne({
+      where: { username },
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
   }
 
-  const secretKey = process.env.JWT_SECRET_KEY || ''
-  const token = jwt.sign({ username}, secretKey, { expiresIn: '1h' });
+  if (!user) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+  let passwordIsValid;
+
+try {
+    passwordIsValid = await bcrypt.compare(password, user.password);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+
+  if (!passwordIsValid) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+  
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
   return res.json ({ token });
 };
 
 const router = Router();
 
 // POST /login - Login a user
-router.post('/login', login);
+router.post('/login', login); 
 
 export default router;
